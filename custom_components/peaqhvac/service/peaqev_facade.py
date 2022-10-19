545dfa381b1f641bab96209128b076bf4114b67d
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-10-19 21:11:16 +0200

    #1

---
+++ custom_components/peaqhvac/service/peaqev_facade.py
@@ -0,0 +1,66 @@
+import logging
+from homeassistant.core import (
+    HomeAssistant
+)
+from enum import Enum
+
+_LOGGER = logging.getLogger(__name__)
+
+class Entities(Enum):
+    Threshold = 0
+    Prediction = 1
+    TotalEnergyAccumulated = 2
+
+ENTITIES = {
+    Entities.Threshold: {
+        "entity":"sensor.peaqev_threshold",
+        "attributes":["start_threshold","stop_threshold"]
+    },
+    Entities.Prediction: {
+        "entity": "sensor.peaqev_prediction",
+        "attributes": []
+    },
+    Entities.TotalEnergyAccumulated: {
+        "entity": "sensor.peaqev_energy_including_car_hourly",
+        "attributes": []
+    }
+}
+
+
+class PeaqevFacade:
+    def __init__(self, hass: HomeAssistant):
+        self._hass = hass
+
+    @property
+    def above_stop_threshold(self) -> bool:
+        data = self._get_state(ENTITIES[Entities.Threshold])
+        if data is not None:
+            return float(data["state"]) > float(data["stop_threshold"])
+        return False
+
+    @property
+    def below_start_threshold(self) -> bool:
+        data = self._get_state(ENTITIES[Entities.Threshold])
+        if data is not None:
+            return float(data["state"]) < float(data["start_threshold"])
+        return False
+
+    def _get_state(self, entity_input: dict):
+        _entity = entity_input["entity"]
+        _attr = entity_input["attributes"] if len(entity_input["attributes"]) > 0 else None
+        return self._get_state_from_hass(_entity, *_attr)
+
+    def _get_state_from_hass(self, entity: str, *attributes) -> dict:
+        _state = self._hass.states.get(entity)
+        ret = {
+            "state": _state.state
+        }
+        if _state is not None:
+            if attributes is not None:
+                try:
+                    for a in attributes:
+                        ret_attr = _state.attributes.get(a)
+                        ret[a] = ret_attr
+                except Exception as e:
+                    _LOGGER.exception(f"{e}")
+        return ret
