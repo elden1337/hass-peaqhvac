60c5e4313cbd31a2a04b009085168f63dfdaac6d
magnuselden <magnu@DESKTOP-ACV2GPT>
2023-11-27 17:13:01 +0100

    try to fix vent boost resets. #92

--- custom_components/peaqhvac/service/hvac/house_ventilation.py
+++ custom_components/peaqhvac/service/hvac/house_ventilation.py
@@ -17,16 +17,26 @@ class HouseVentilation:
         self._hvac = hvac
         self._wait_timer_boost = WaitTimer(timeout=WAITTIMER_VENT, init_now=False)
         self._current_vent_state: bool = False
+        self._latest_seen_fan_speed: float = 0
         async_track_time_interval(self._hvac.hub.state_machine, self.async_check_vent_boost, timedelta(seconds=30))
 
     @property
     def vent_boost(self) -> bool:
+        self._check_hvac_fan_speed()
         return self._current_vent_state
 
     @vent_boost.setter
     def vent_boost(self, val) -> None:
         if isinstance(val, bool):
             self._current_vent_state = val
+
+    def _check_hvac_fan_speed(self) -> None:
+        if self._hvac.fan_speed != self._latest_seen_fan_speed:
+            _LOGGER.debug("hvac ventilation speed changed from %s to %s", self._latest_seen_fan_speed, self._hvac.fan_speed)
+            if self._latest_seen_fan_speed > self._hvac.fan_speed:
+                """Decreased"""
+                self._current_vent_state = False
+            self._latest_seen_fan_speed = self._hvac.fan_speed
 
     async def async_check_vent_boost(self, caller=None) -> None:
         if self._hvac.hub.sensors.temp_trend_indoors.samples > 0 and time.time() - self._wait_timer_boost.value > WAITTIMER_VENT:
@@ -43,7 +53,7 @@ class HouseVentilation:
             self._hvac.hub.sensors.average_temp_outdoors.value < self._hvac.hub.options.heating_options.very_cold_temp
             ]):
             if self.vent_boost:
-                _LOGGER.debug(f"recovered dm or very cold. stopping went boost. dm: {self._hvac.hvac_dm} > {self._hvac.hub.options.heating_options.low_degree_minutes + 200}, temp: {self._hvac.hub.sensors.average_temp_outdoors.value}")
+                _LOGGER.debug(f"recovered dm or very cold. stopping went boost. dm: {self._hvac.hvac_dm} > {self._hvac.hub.options.heating_options.low_degree_minutes + 100}, temp: {self._hvac.hub.sensors.average_temp_outdoors.value}")
                 self.vent_boost = False
                 self._hvac.hub.observer.broadcast(ObserverTypes.UpdateOperation)
 
