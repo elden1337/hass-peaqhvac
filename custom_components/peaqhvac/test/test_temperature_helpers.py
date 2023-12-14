ea44dff024e2af54179fed5079f38e0caff742e3
magnuselden <magnu@DESKTOP-ACV2GPT>
2023-12-14 18:04:14 +0100

    temp tests

--- custom_components/peaqhvac/test/test_temperature_helpers.py
+++ custom_components/peaqhvac/test/test_temperature_helpers.py
@@ -1,7 +1,7 @@
 import pytest
 
 from ..service.hub.target_temp import adjusted_tolerances
-from ..service.hvac.house_heater.temperature_helper import get_temp_extremas
+from ..service.hvac.house_heater.temperature_helper import get_temp_extremas, get_tempdiff_inverted
 
 MINTOLERANCE = 0.2
 MAXTOLERANCE = 0.5
@@ -20,3 +20,26 @@ def test_temp_extremas_one_positive():
 def test_temp_extremas_one_negative():
     ter = get_temp_extremas(0, [0, 0, 0, 0, -1, 0, 0, 0, 0], _current_tolerances)
     assert ter == -0.5
+
+
+def test_tempdiff_cold():
+    tempdiff = -0.5
+    ret1 = get_tempdiff_inverted(0, tempdiff, _current_tolerances)
+    assert ret1 == 2
+    ret2 = get_tempdiff_inverted(-1, tempdiff, _current_tolerances)
+    assert ret2 == 2
+    ret3 = get_tempdiff_inverted(-2, tempdiff, _current_tolerances)
+    assert ret3 == 1
+    ret4 = get_tempdiff_inverted(-3, tempdiff, _current_tolerances)
+    assert ret4 == 0
+
+def test_tempdiff_hot():
+    tempdiff = 0.5
+    ret1 = get_tempdiff_inverted(0, tempdiff, _current_tolerances)
+    assert ret1 == -1
+    ret2 = get_tempdiff_inverted(-1, tempdiff, _current_tolerances)
+    assert ret2 == -1
+    ret3 = get_tempdiff_inverted(-2, tempdiff, _current_tolerances)
+    assert ret3 == -1
+    ret4 = get_tempdiff_inverted(-3, tempdiff, _current_tolerances)
+    assert ret4 == -1
