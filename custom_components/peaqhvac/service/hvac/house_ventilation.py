cf81cb294712475672d5ec23441e5f84d16bf8a7
Magnus Eldén <elden1337@users.noreply.github.com>
2024-05-14 22:14:56 +0200

    Update house_ventilation.py

--- custom_components/peaqhvac/service/hvac/house_ventilation.py
+++ custom_components/peaqhvac/service/hvac/house_ventilation.py
@@ -76,6 +76,7 @@ class HouseVentilation:
         return all(
                     [
                         self._hvac.hub.sensors.get_tempdiff() > 3,
+                        self._hvac.hub.sensors.get_tempdiff_in_out() > 4,
                         self._hvac.hub.sensors.temp_trend_indoors.gradient >= 0,
                         self._hvac.hub.sensors.temp_trend_outdoors.gradient >= 0,
                         self._hvac.hub.sensors.average_temp_outdoors.value >= self._hvac.hub.options.heating_options.outdoor_temp_stop_heating,
@@ -88,7 +89,7 @@ class HouseVentilation:
                     [
                         self._hvac.hub.sensors.get_tempdiff_in_out() > 4,
                         self._hvac.hub.sensors.average_temp_outdoors.value >= self._hvac.hub.options.heating_options.outdoor_temp_stop_heating,
-                        datetime.now().hour in [23,0,1,2,3,4,5],
+                        datetime.now().hour in [21,22,23,0,1,2,3,4,5,6],
                         self._hvac.hub.sensors.set_temp_indoors.preset != HvacPresets.Away,
                     ]
                 )
