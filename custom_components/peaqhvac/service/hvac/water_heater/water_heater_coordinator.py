27f45443def680e9f197bc0475b6920eb81659b7
magnuselden <magnu@DESKTOP-ACV2GPT>
2024-08-27 21:13:07 +0200

    cleanup

--- custom_components/peaqhvac/service/hvac/water_heater/water_heater_coordinator.py
+++ custom_components/peaqhvac/service/hvac/water_heater/water_heater_coordinator.py
@@ -70,8 +70,11 @@ class WaterHeater(IHeater):
         return "-"
 
     def import_latest_boost_call(self, strtime):
-        struct_time = time.strptime(strtime, "%Y-%m-%d %H:%M")
-        new = time.mktime(struct_time)
+        try:
+            struct_time = time.strptime(strtime, "%Y-%m-%d %H:%M")
+            new = time.mktime(struct_time)
+        except ValueError as e:
+            new = 0
         current = self.model.latest_boost_call
         self.model.latest_boost_call = max(new, current)
 
